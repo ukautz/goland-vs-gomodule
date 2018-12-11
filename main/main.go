a1caab21ee0a66b9f6c81992363449f928a0471d
Ulrich Kautz <ulrich.kautz@scout24.com>
2018-12-11 17:50:51 +0100

    Showcasing bug

---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/ukautz/goland-vs-gomodule"
+	"github.com/ukautz/goland-vs-gomodule/foo"
+)
+
+func main() {
+	foo.Foo()
+	toplevel.Hello()
+}
---
+++ toplevel.go
@@ -0,0 +1,7 @@
+package toplevel
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello")
+}
